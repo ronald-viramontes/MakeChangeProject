1963a2b27d3c8ed44813aad63d46da1083544ea9
ronald-viramontes <ronald.viramontes@gmail.com>
2021-07-25 12:52:29 -0400

    Finishing touches on the MakeChange App

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -7,22 +7,21 @@ public class MakeChange {
 		
 										//STORE FRONT//
 		System.out.println(" -----------------------   Welcome to RV Liquidators   ------------------------");
+		System.out.println("                             • Se habla español •                               ");
 		System.out.println("\n                 Please enter the purchase price of the item        ");
 			double priceItem = kb.nextDouble();
 		System.out.println("        Please enter the amount of money you will be paying with         ");
 			double moneyTendered = kb.nextDouble();
 					
-									//CURRENCY VARIABLES//
-		double tenDollar = 10;		String tenDollarBill = " ten dollar bill";
-		double fiveDollar = 5;		String fiveDollarBill = " five dollar bill";
-		double oneDollar = 1;		String oneDollarBill = " one dollar bill";
-		double quarter = .25;		String quarterCoin = " quarters";
-		double dime = .1;			String dimeCoin = " dimes"; 
-		double nickel = .05;		String nickelCoin = " nickels";
-		double penny = .01;			String pennyCoin = " pennies";
+									//CURRENCY STRINGS VARIABLES//
+//				String twentyDollarBill = twentyDollarCount + " twenty dollar bill(s) "; 
+//				String tenDollarBill = tenDollarCount + " ten dollar bill(s) ";	
+//				String fiveDollarBill = fiveDollarCount + " five dollar bill(s) ";
+//				String oneDollarBill = oneDollarCount + " one dollar bill(s) ";	String quarterCoin = quarterCount + " quarter(s) ";
+//				String dimeCoin = dimeCount + " dime(s) "; String nickelCoin = nickelCount + " nickel(s) ";	String pennyCoin = pennyCount + " pennies ";
 					
-					//ITEM COST & AMOUNT TENDERED FORMULA//
-			double changeReturned = (moneyTendered - priceItem);
+								//ITEM COST & AMOUNT TENDERED FORMULA//
+						double changeReturned = (moneyTendered - priceItem);
 
 			//-----------------------Bill & Coin Count Variables--------------------//
 										//BILLS//
@@ -46,19 +45,22 @@ public class MakeChange {
 						int pennyCount = coinChangeReturned/1;
 						coinChangeReturned %= 1;
 						
+						String twentyDollarBill = twentyDollarCount + " twenty dollar bill(s) "; 
+						String tenDollarBill = tenDollarCount + " ten dollar bill(s) ";	
+						String fiveDollarBill = fiveDollarCount + " five dollar bill(s) ";
+						String oneDollarBill = oneDollarCount + " one dollar bill(s) ";	String quarterCoin = quarterCount + " quarter(s) ";
+						String dimeCoin = dimeCount + " dime(s) "; String nickelCoin = nickelCount + " nickel(s) ";	String pennyCoin = pennyCount + " pennies ";
 								//STATEMENTS IF AMOUNT TENDERED IS EQUAL TO THE ITEM AMOUNT & LESS THAN THE ITEM AMOUNT//
 			if (moneyTendered < priceItem) {
-				System.out.println("Cost: " + priceItem + " Amount Tendered: " + moneyTendered + " The amount tendered is less than the purchase price!");		
+				System.out.println("Cost: $" + priceItem + " Amount Tendered: $" + moneyTendered + " The amount tendered is less than the purchase price!");		
 			} else if (moneyTendered == priceItem) {
-				System.out.println("Cost: " + priceItem + " Amount Tendered: " + moneyTendered + " You have provided exact change!" + 
+				System.out.println("Cost: $" + priceItem + " Amount Tendered: $" + moneyTendered + " You have provided exact change!" + 
 						"\n Thank you for shopping with us!  Have a great day!");
 			} else {
-				
-				System.out.println("\t  Cost: " + priceItem + " Amount Tendered: " + moneyTendered + " Change: " + changeReturned2);
-				System.out.println("*** Return Bills: " + tenDollarCount + tenDollarBill + " " + fiveDollarCount + fiveDollarBill + " " + oneDollarCount + oneDollarBill + " ***");
-				System.out.println("\t*** Return Coins: " + quarterCount + quarterCoin + " " + dimeCount + dimeCoin + " " + nickelCount + nickelCoin + " " + pennyCount + pennyCoin + " ***");
-				
-							
+								//SYSOUT FOR ITEM COST, AMOUNT TENDERED, CHANGE DUE --> BILLS OWED & COINS OWED//
+				System.out.println("\t  Cost: $" + priceItem + " Amount Tendered: $" + moneyTendered + " Change: $" + changeReturned2);
+				System.out.println("\n • Return Bills: " + twentyDollarBill + tenDollarBill + "\n\t\t    " + fiveDollarBill + oneDollarBill);
+				System.out.println("\n • Return Coins: " + quarterCoin + dimeCoin + nickelCoin + pennyCoin);					
 				System.out.println("\n\t--------Thank you for shopping with RV Liquidators--------");
 			} kb.close();		
 		}
