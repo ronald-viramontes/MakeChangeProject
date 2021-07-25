fce8aba4226b092426bbe1a8ad5c5c06062f1ecc
ronald-viramontes <ronald.viramontes@gmail.com>
2021-07-25 18:24:09 -0400

    Added if(s) to only display bills & coins that are dispensed

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -6,11 +6,11 @@ public class MakeChange {
 		Scanner kb = new Scanner(System.in);
 		
 														//STORE FRONT//
-		System.out.println(" -----------------------   Welcome to RV Liquidators   ------------------------");
-		System.out.println("                             • Se habla español •                               ");
-		System.out.println("\n                 Please enter the purchase price of the item        ");
+		System.out.println("\t\t -----------------------   Welcome to RV Liquidators   ------------------------");
+		System.out.println("\t\t                             • Se habla español •                               ");
+		System.out.println("\n\t\t                 Please enter the purchase price of the item        ");
 			double priceItem = kb.nextDouble();
-		System.out.println("        Please enter the amount of money you will be paying with         ");
+		System.out.println("\t\t        Please enter the amount of money you will be paying with         ");
 			double moneyTendered = kb.nextDouble();
 							//ITEM COST & AMOUNT TENDERED FORMULA//
 						double changeReturned = (moneyTendered - priceItem);
@@ -37,11 +37,14 @@ public class MakeChange {
 						int pennyCount = coinChangeReturned/1;
 						coinChangeReturned %= 1;
 											//STRING CONCATENATION// 
-						String twentyDollarBill = twentyDollarCount + " twenty dollar bill(s) "; 
-						String tenDollarBill = tenDollarCount + " ten dollar bill(s) ";	
-						String fiveDollarBill = fiveDollarCount + " five dollar bill(s) ";
-						String oneDollarBill = oneDollarCount + " one dollar bill(s) ";	String quarterCoin = quarterCount + " quarter(s) ";
-						String dimeCoin = dimeCount + " dime(s) "; String nickelCoin = nickelCount + " nickel(s) ";	String pennyCoin = pennyCount + " pennies ";
+						String twentyDollarBill = "\t" + twentyDollarCount + " twenty dollar bill(s) "; 
+						String tenDollarBill = "\t" + tenDollarCount + " ten dollar bill(s) ";	
+						String fiveDollarBill = "\t" + fiveDollarCount + " five dollar bill(s) ";
+						String oneDollarBill = "\t" + oneDollarCount + " one dollar bill(s) ";	
+						String quarterCoin = "\t" + quarterCount + " quarter(s) ";
+						String dimeCoin = "\t" + dimeCount + " dime(s) "; 
+						String nickelCoin = "\t" + nickelCount + " nickel(s) ";	
+						String pennyCoin = pennyCount + " pennies ";
 								//STATEMENTS IF AMOUNT TENDERED IS EQUAL TO THE ITEM AMOUNT & LESS THAN THE ITEM AMOUNT//
 			if (moneyTendered < priceItem) {
 				System.out.println("Cost: $" + priceItem + " Amount Tendered: $" + moneyTendered + " The amount tendered is less than the purchase price!");		
@@ -50,11 +53,39 @@ public class MakeChange {
 						"\n Thank you for shopping with us!  Have a great day!");
 			} else {
 								//SYSOUT FOR ITEM COST, AMOUNT TENDERED, CHANGE DUE --> BILLS OWED & COINS OWED//
-				System.out.println("\t  Cost: $" + priceItem + " Amount Tendered: $" + moneyTendered + " Change: $" + changeReturned2);
-				System.out.println("\n • Return Bills: " + twentyDollarBill + tenDollarBill + "\n\t\t    " + fiveDollarBill + oneDollarBill);
-				System.out.println("\n • Return Coins: " + quarterCoin + dimeCoin + nickelCoin + pennyCoin);					
-				System.out.println("\n\t--------Thank you for shopping with RV Liquidators--------");
-			} kb.close();		
+				System.out.println("\t\t\t  Cost: $" + priceItem + " Amount Tendered: $" + moneyTendered + " Change: $" + changeReturned2);
+				
+										//CAN ENABLE LINES 58 & 59 TO DISPLAY ALL BILLS AND COINS EVEN IF QUANTITY IS ZERO//
+//				System.out.println("\n • Return Bills: " + twentyDollarBill + tenDollarBill + "\n\t\t    " + fiveDollarBill + oneDollarBill);
+//				System.out.println("\n • Return Coins: " + quarterCoin + dimeCoin + nickelCoin + pennyCoin);					
+			} 
+			if (twentyDollarCount > 0) {
+			      System.out.println(twentyDollarBill);
+			        }
+			     if (tenDollarCount > 0) {
+			             System.out.print(tenDollarBill);
+			        }
+				if (fiveDollarCount > 0) {
+			      System.out.print(fiveDollarBill);
+			      }
+				if (oneDollarCount > 0) {
+			      System.out.print(oneDollarBill);
+			      }
+				if (quarterCount > 0) {
+			      System.out.print(quarterCoin);
+			      }
+			    if (dimeCount > 0) {
+					System.out.print(dimeCoin);
+			      }
+			    if(nickelCount > 0) {
+					System.out.print(nickelCoin);
+			      }
+			    if (pennyCount > 0) {
+					System.out.print(pennyCoin);
+			      }
+			    System.out.println("\n\n\t--------Thank you for shopping with RV Liquidators--------");
+			
+			kb.close();		
 		}
 	} 
 
